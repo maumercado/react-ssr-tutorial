533013f225c99dbcd4d46adb771ff7c035da66ce
Mauricio Mercado <mau@maumercado.com>
2018-02-14 22:10:22 -0800

    Set up hydration on client side and added server side id to main dom

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,7 @@
 // startup point for the client side application
+// Treat this file as a normal entry point for a react app
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./components/Home";
+
+ReactDOM.hydrate(<Home />, document.querySelector("#root"));
