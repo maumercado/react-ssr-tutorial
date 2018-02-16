af87d731f9a11e258c6fc6f82ae32cfa89106ba5
Mauricio Mercado <mau@maumercado.com>
2018-02-15 17:53:28 -0800

    Setup router file for client side with Browser Router support

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,12 @@
 // Treat this file as a normal entry point for a react app
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
-ReactDOM.hydrate(<Home />, document.querySelector("#root"));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes />
+    </BrowserRouter>,
+    document.querySelector("#root")
+);
