364a994807c665f7cd261e0f244347d0dad39729
Mauricio Mercado <mau@maumercado.com>
2018-02-15 20:58:22 -0800

    Add matchroutes function on server catch all routes, to detect which of our client side components will be rendered based on the path requested.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { matchRoutes } from "react-router-config";
+import Routes from "./client/Routes";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
 
@@ -8,6 +10,7 @@ app.use(express.static("public"));
 
 app.get("*", (req, res) => {
     const store = createStore();
+    matchRoutes(Routes, req.path);
     res.send(renderer(req, store));
 });
 
