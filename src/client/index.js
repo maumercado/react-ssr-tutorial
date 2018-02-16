849f8de3f3a4c4631f032438016d40a0a4e33559
Mauricio Mercado <mau@maumercado.com>
2018-02-15 18:27:36 -0800

    Initial creation of store in both client and server side

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,20 @@
 // startup point for the client side application
 // Treat this file as a normal entry point for a react app
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./Routes";
 
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.querySelector("#root")
 );
