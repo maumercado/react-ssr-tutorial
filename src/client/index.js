228b43bb23e6b18f76c3ddfa49449b73c6f2c6a4
Mauricio Mercado <mau@maumercado.com>
2018-02-15 20:54:42 -0800

    Refactor routes using react-router-config which allows us to match routes on both client and server side for some ssr goodies

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
 import Routes from "./Routes";
 
 import reducers from "./reducers";
@@ -15,7 +16,7 @@ const store = createStore(reducers, {}, applyMiddleware(thunk));
 ReactDOM.hydrate(
     <Provider store={store}>
         <BrowserRouter>
-            <Routes />
+            <div>{renderRoutes(Routes)}</div>
         </BrowserRouter>
     </Provider>,
     document.querySelector("#root")
