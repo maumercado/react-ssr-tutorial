9c318493386aa54ba1b861163e38a5f69717d335
Mauricio Mercado <mau@maumercado.com>
2018-02-19 18:12:21 -0800

    Adding styles

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>
-            <div>I'm the Home component</div>
-            <button onClick={() => console.log("Hi there")}>Press me</button>
+        <div className="center-align" style={{ marginTop: "200px" }}>
+            <h3>Welcome</h3>
+            <p>
+                This page is being rendered from the server, and then react
+                takes over!
+            </p>
         </div>
     );
 };
