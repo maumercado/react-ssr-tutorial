8aaca81970e0e02751aa435b1bedeede496f714d
Mauricio Mercado <mau@maumercado.com>
2018-02-19 19:41:32 -0800

    Setup initial redux boilerplate for admins

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,5 +1,4 @@
 export const FETCH_USERS = "fetch_users";
-
 export const fetchUsers = () => async (dispatch, getState, api) => {
     const res = await api.get(`/users`);
     dispatch({
@@ -13,6 +12,15 @@ export const fetchCurrentUser = () => async (dispatch, getState, api) => {
     const res = await api.get("/current_user");
     dispatch({
         type: FETCH_CURRENT_USER,
+        payload: res.data
+    });
+};
+
+export const FETCH_ADMINS = "fetch_admins";
+export const fetchAdmins = () => async (dispatch, getState, api) => {
+    const res = await api.get("/admins");
+    dispatch({
+        type: FETCH_ADMINS,
         payload: res.data
     });
 };
--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import usersReducers from "./users_reducers";
 import authReducers from "./auth_reducers";
+import adminsReducers from "./admins_reducers";
 
 export default combineReducers({
     users: usersReducers,
-    auth: authReducers
+    auth: authReducers,
+    admins: adminsReducers
 });
